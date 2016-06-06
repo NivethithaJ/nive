7856376d82c20255b24219b0fa0d64d17bfb6695
NivethithaJ <nivejegan318@gmail.com>
2016-06-06 11:35:34 +0530

    Create oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+   long long int number;
+   scanf("%lld",&number);
+   if(number%2==0)
+   printf("Even number");
+   else
+   printf("Odd Number");
+	return 0;
+}
